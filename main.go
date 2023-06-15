f57ec9b0f2271fdf4a16cf0327d792a5a387fa6d
Lei <random2035@qq.com>
2023-06-15 16:24:50 +0800

    1

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import "regexp"
-
 func main() {
-	re := regexp.MustCompile(`0{3,5}|1{3,5}|2{3,5}|3{3,5}|4{3,5}|5{3,5}|6{3,5}|7{3,5}|8{3,5}|9{3,5}|^520|^1314|.520|.1314`)
-
-	for _, s := range []string{`00000123`, `100000654`, `54000000`} {
-		println(re.MatchString(s))
-	}
 
 }
--- tencent_svc/im_img_audit_test.go
+++ tencent_svc/im_img_audit_test.go
@@ -34,3 +34,24 @@ func TestIMAuditImage(t *testing.T) {
 
 	log.Printf("resp: %+v", *resp)
 }
+
+func TestIMAuditText(t *testing.T) {
+	b, err := os.ReadFile("../_ignore/im_audit.json")
+	if err != nil {
+		log.Println("read file err", err.Error())
+		return
+	}
+	sdkConf := new(imSdkConf)
+	err = json.Unmarshal(b, sdkConf)
+	if err != nil {
+		log.Println("Unmarshal err", err.Error())
+		return
+	}
+	resp, err := IMAuditText("习大大", sdkConf.AppId, sdkConf.Key, sdkConf.Identifier)
+	if err != nil {
+		log.Printf("Unmarshal err: %+v", err.Error())
+		return
+	}
+
+	log.Printf("resp: %+v", *resp)
+}
