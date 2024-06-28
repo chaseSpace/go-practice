a8b050b25150e46d341d253edf44f21ae224f8a1
chasespace <no@ex.com>
2024-06-28 10:27:10 +0800

    1

--- .githooks/util.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-__exitWith() {
-    tips=$1
-    if [ ! -z "$tips" ]; then
-      echo "********** $tips ***********"
-    fi
-    exit 1
-}
--- main_test.go
+++
@@ -1,7 +0,0 @@
-package main_test
-
-import "testing"
-
-func TestMain(m *testing.M) {
-
-}
