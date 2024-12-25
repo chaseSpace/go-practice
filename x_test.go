9fba3ac27377bd7f3696ac6a34bbd09e737d7aa7
chasespace <any@qq.com>
2024-12-25 21:19:25 +0800

    1

--- x_test.go
+++ x_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestX(t *testing.T) {
-	d, _ := time.ParseDuration("7s")
-	assert.Equal(t, d, time.Second*7)
+	d, _ := time.ParseDuration("7m")
+	assert.Equal(t, d, time.Minute*7)
+
+	d, _ = time.ParseDuration("7h7s")
+	assert.Equal(t, d, time.Hour*7+time.Second*7)
+
+	d, err := time.ParseDuration("7d")
+	t.Log(err) /// time: unknown unit "d" in duration "7d"
 }
