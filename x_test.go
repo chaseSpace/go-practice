9868c4bf40979d69224c0a185d93189767fdf33a
chasespace <no@ex.com>
2025-03-04 15:56:31 +0800

    +printReadableTypeValue2

--- x_test.go
+++ x_test.go
@@ -6,31 +6,31 @@ import (
 	"testing"
 )
 
-// printReadableTypeValue returns a string representation of the type and value of the given value.
-func printReadableTypeValue(value interface{}) string {
-
+// PrintReadableTypeValue returns a string representation of the type and value of the given value.
+// Todo: to compatible with struct type that contains pointer fields.
+func PrintReadableTypeValue(value interface{}) string {
 	v := reflect.ValueOf(value)
 	t := v.Type()
 
-	var valueStr string
-	if v.Kind() == reflect.Ptr {
-		if v.IsNil() {
-			valueStr = "nil"
-		} else {
-			valueStr = fmt.Sprintf("%v", v.Elem().Interface())
+	if v.Kind() != reflect.Ptr {
+		return fmt.Sprintf("(%s)(%v)", t, v.Interface())
+	}
+
+	var typeStr, valueStr string
+	for elem := v; ; elem = elem.Elem() {
+		if elem.Kind() != reflect.Ptr {
+			typeStr += elem.Type().String()
+			valueStr = fmt.Sprintf("(%+v)", elem.Interface())
+			break
+		} else if elem.IsNil() {
+			typeStr += elem.Type().String()
+			valueStr = "(nil)"
+			break
 		}
-	} else {
-		valueStr = fmt.Sprintf("%v", v.Interface())
+		typeStr += "*"
 	}
 
-	var typeStr string
-	if v.Kind() == reflect.Ptr {
-		typeStr = fmt.Sprintf("%s(%s)", t, valueStr)
-	} else {
-		typeStr = fmt.Sprintf("%s(%s)", t, valueStr)
-	}
-
-	return typeStr
+	return fmt.Sprintf("(%s)%s", typeStr, valueStr)
 }
 
 func TestX(t *testing.T) {
