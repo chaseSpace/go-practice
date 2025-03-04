5fd3fff9b1a2bb8ad0924ce43573f0bd76f14a50
chasespace <no@ex.com>
2025-03-04 13:02:18 +0800

    +printReadableTypeValue

--- x_test.go
+++ x_test.go
@@ -1,8 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 )
+
+// printReadableTypeValue returns a string representation of the type and value of the given value.
+func printReadableTypeValue(value interface{}) string {
+
+	v := reflect.ValueOf(value)
+	t := v.Type()
+
+	var valueStr string
+	if v.Kind() == reflect.Ptr {
+		if v.IsNil() {
+			valueStr = "nil"
+		} else {
+			valueStr = fmt.Sprintf("%v", v.Elem().Interface())
+		}
+	} else {
+		valueStr = fmt.Sprintf("%v", v.Interface())
+	}
+
+	var typeStr string
+	if v.Kind() == reflect.Ptr {
+		typeStr = fmt.Sprintf("%s(%s)", t, valueStr)
+	} else {
+		typeStr = fmt.Sprintf("%s(%s)", t, valueStr)
+	}
+
+	return typeStr
+}
 
 func TestX(t *testing.T) {
 	g := func() int { return 10 }
