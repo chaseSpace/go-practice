860035f72568e56dd7ca699b2d12f24396cdc200
lx <tmp@invalid.com>
2026-02-03 21:28:27 +0800

    1

--- shell_script_demo/install_go.sh
+++ shell_script_demo/install_go.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-
+set -e
 
 #注意
 #  1. 若已安装go，请从PATH中删除已有go版本的ENV，否则脚本执行后仍然会识别旧版
 #  2. 请不要在goland的terminal中执行此脚本，因为此处打开的shell中的PATH会自动设置当前goland配置的go版本，导致脚本执行完后，本来已经安装成功，但因PATH设置不正确导致未能识别新版本go。
 
-version=go1.21.12
+version=go1.23.12
 
 target=$version.linux-amd64.tar.gz
 
@@ -14,7 +14,7 @@ GO_INSTALL='/usr/local'
 BASHRC='/root/.bashrc'
 ZSHRC='/root/.zshrc'
 
-if [ `uname` == "Darwin" ]; then
+if [ `uname` = "Darwin" ]; then
   echo "This is macOS"
   GOPATH=$HOME/gopath
   GO_INSTALL=$HOME
@@ -27,7 +27,7 @@ PATH=$(echo $PATH | sed 's/ /\\ /g')  # 将PATH中的空格转义，否则source
 
 # mac遇到 wget 无法建立SSL连接，请修改代理为直连。
 now=$(pwd) && cd $GO_INSTALL && \
-wget https://studygolang.com/dl/golang/$target && \
+wget --no-check-certificate https://studygolang.com/dl/golang/$target && \
 tar xzf $target && \
 rm -rf $target && \
 mkdir -p $GOPATH && mkdir -p $GOPATH/bin
